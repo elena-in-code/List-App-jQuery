083cd80d4b75e10a99bfec610d0dec99ee62eab7
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2017-09-13 10:05:29 +0200

    coments js
    added coments on jquery asset

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,19 @@
 //adds item  to list on button click
 $('#add').click(function() {
   var value = $('#item').val();
+  //clear after new li it is added to the ul
+  $('#item').val("");
+  //create a new li and add to ul
   $('ul').append('<li>' + value + '</li>');
 });
+
 //adds item to list on enter key hit
 $("#item").keyup(function(event){
     if(event.keyCode == 13){
       var value = $('#item').val();
+      //clear after new li it is added to the ul
+      $('#item').val("");
+      //create a new li and add to ul
       $('ul').append('<li>' + value + '</li>');
     }
 });
