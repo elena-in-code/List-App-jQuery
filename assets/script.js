a35d747cb56b2aa1f2dbf13bb4b0e383d0550eaf
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2017-09-13 10:51:03 +0200

    text input not blank allowed
    added an alert for the input not to be blank

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,16 @@
+$(document).ready();
 //adds item  to list on button click
 $('#add').click(function() {
   var value = $('#item').val();
+  //alert not leave input blank
+  if($('#item').val() == ''){
+    alert("Please, don't leave the text box empty, write the name of those beautiful countries you've visited :D");
+ //if input it is not blank:
+}else{
   //clear after new li it is added to the ul
   $('#item').val("");
   //create a new li and add to ul
   $('ul').append('<li>' + value + '</li>');
+  }
 });
 
-//adds item to list on enter key hit
-$("#item").keyup(function(event){
-    if(event.keyCode == 13){
-      var value = $('#item').val();
-      //clear after new li it is added to the ul
-      $('#item').val("");
-      //create a new li and add to ul
-      $('ul').append('<li>' + value + '</li>');
-    }
-});
